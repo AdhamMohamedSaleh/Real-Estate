7409f7f00fd049701a69d105856648dd97249ca3
AdhamMoSaleh <adham.work24@gmail.com>
2025-05-28 10:40:35 +0300

    Your update message

--- src/contexts/currencyContext.jsx
+++ src/contexts/currencyContext.jsx
@@ -34,6 +34,7 @@ export function CurrencyProvider({ children }) {
   );
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export function useCurrency() {
   return useContext(CurrencyContext);
 }
