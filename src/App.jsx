39506a6c4ef311119d76efdfae44c1970e299b4d
AdhamMoSaleh <adham.work24@gmail.com>
2025-05-28 11:20:06 +0300

    Your update message

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import AppLayout from "./ui/AppLayout.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import PropertyDetails from "./pages/PropertyDetails.jsx";
+import { CurrencyProvider } from "./contexts/CurrencyContext";
+
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route element={<AppLayout />}>
-          <Route index element={<Navigate to="/dashboard" />} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="property/:id" element={<PropertyDetails />} />
-        </Route>
-      </Routes>
+      <CurrencyProvider>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate to="/dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="property/:id" element={<PropertyDetails />} />
+          </Route>
+        </Routes>
+      </CurrencyProvider>
     </BrowserRouter>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/tailwind.css";
-import { CurrencyProvider } from "./contexts/CurrencyContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <CurrencyProvider>
-      <App />
-    </CurrencyProvider>
+    <App />
   </StrictMode>
 );
